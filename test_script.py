b1324d28b0eafda906dc1142f821f5ade969bcc3
utkrishtsharma <utkrishtsharma93@gmail.com>
2025-05-13 03:27:57 +0530

    Update test_script.py

--- test_script.py
+++ test_script.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import time
 
-# Configure Chrome options
-options = Options()
-options.add_argument("--headless")  # Run in headless mode
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
+# Set up Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Run in headless mode
 
 # Connect to the Selenium server
 driver = webdriver.Remote(
     command_executor='http://localhost:4444/wd/hub',
-    options=options
+    options=chrome_options
 )
 
-# Navigate to a website and print the title
+# Navigate to a website
 driver.get("https://www.example.com")
-print("Page title is:", driver.title)
+
+# Perform actions
+print(driver.title)
 
 # Clean up
 driver.quit()
